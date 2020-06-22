9a4eb04f368cd2ece6e267bcc8b6dfbfc18758ae
Askye <Askye@outlook.fr>
2020-06-22 11:24:09 +0200

    Update change_name.py

--- change_name.py
+++ change_name.py
@@ -1,14 +1,26 @@
 import subprocess
 
+def take_extension (name) :
+	lst = name.split('.')
+	return '.'+lst[-1]
+
+
 def change_name(path, saison, debut, new_name) :
 	i = debut
 	res = subprocess.run("ls " + path, shell = True, capture_output=True, encoding = 'UTF-8')
 	lst = res.stdout.split('\n')
+	lst.pop()
+
 	for elem in lst :
 		print (elem)
-		subprocess.run("mv " +path+"'"+elem+"'"+ " "+ path +
-			"'"+new_name+str(saison)+"'"+str(i).zfill(2)+".mkv", shell=True)
+		ext = take_extension (elem)
+
+		subprocess.run("mv " + path + "'" + elem + "'" + " " + 
+			path + "'" + new_name + " S" + str(saison).zfill(2) + "E" + str(i).zfill(2) + ext + "'"
+			, shell=True)
+
 		i += 1
 
-#change_name("/mnt/d/'Shingeki No Kyojin'/'Shingeki No Kyojin S03/'", 3, 38)
-change_name("/mnt/d/'Fullmetal Alchemist Brotherhood'/", " ", 1, "Fullmetal Alchemist Brotherhood")
+
+change_name("/mnt/d/'Shingeki No Kyojin'/'Shingeki No Kyojin S03'/", 3, 1, "Shingeki No Kyojin")
+#change_name("/mnt/d/'Fullmetal Alchemist Brotherhood'/", " ", 1, "Fullmetal Alchemist Brotherhood")
